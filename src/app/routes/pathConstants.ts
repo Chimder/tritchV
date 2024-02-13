48483cfee239020a46b972455f8042c845ef527b
Chimder <127688661+Chimder@users.noreply.github.com>
2024-02-13 13:37:18 +0200

    init vite

---
+++ src/app/routes/pathConstants.ts
@@ -0,0 +1,9 @@
+class DASHBOARD {
+  private root = '/'
+
+  HOME = this.root
+
+  STREAMER = `${this.root}/streamer/:id`
+}
+
+export const PATH = new DASHBOARD()
---
+++ src/pages/home.tsx
@@ -0,0 +1,42 @@
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
+import { useLoaderData } from 'react-router-dom'
+
+import { Button } from '@/components/ui/button'
+import { DialogInput } from '@/components/dialog-search'
+import EmblaCarousel from '@/components/EmblaCarousel'
+
+function Home() {
+  const games = useLoaderData()
+  console.log('GAME', games)
+  return (
+    <main className="h-[2000px] overflow-hidden">
+      <section className="w-full">
+        <div className="flex flex-col items-center justify-end bg-background pb-10 pt-24 md:py-12">
+          <div className="flex flex-col items-center justify-center ">
+            <h1 className="pb-4 pt-12 text-9xl xl:text-6xl md:pt-16 md:text-4xl ">
+              Discover Twitch Vods
+            </h1>
+            <p className="pb-10 text-center text-2xl lg:text-base md:pb-6 md:text-sm">
+              Watch twitch past broadcasts streams Start by Typing twitch username...
+            </p>
+          </div>
+          <DialogInput>
+            <Button className="bg-button-foreground  px-40 text-xl text-text text-white lg:text-base md:px-32 md:text-sm sm:px-28">
+              <MagnifyingGlassIcon className="h-6 w-6 pr-1" />
+              Search Steamer
+            </Button>
+          </DialogInput>
+        </div>
+      </section>
+      <section className="relative flex w-full flex-col items-center justify-center rounded-2xl ">
+        <div>
+          <h1 className="pb-3 text-7xl text-white xl:text-6xl md:text-4xl">Top streams Now</h1>
+        </div>
+        <EmblaCarousel slides={games} />
+      </section>
+      <section></section>
+    </main>
+  )
+}
+
+export default Home
