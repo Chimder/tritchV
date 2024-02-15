8c853b5e00aac3eb39f3ab7162f30a03f710e6a9
Chimder <127688661+Chimder@users.noreply.github.com>
2024-02-15 21:41:24 +0200

    new orval

--- orval.config.js
+++ orval.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   main: {
     // input: "./src/shared/api/swagger.yaml", // Укажите URL вашего Swagger-документа
-    input: 'http://[::1]:4000/api-yaml',
+    input: 'https://sore-gray-pelican-wear.cyclic.app/api-yaml',
     output: {
       target: './src/shared/api/generated.ts',
       override: {
