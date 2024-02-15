4482c4f7f45675c403e553e237c0aac46ab85cc0
Chimder <127688661+Chimder@users.noreply.github.com>
2024-02-15 22:13:48 +0200

    try

--- src/shared/api/axios.instance.ts
+++ src/shared/api/axios.instance.ts
@@ -1,23 +1,55 @@
+// import Axios, { AxiosError, AxiosRequestConfig } from 'axios'
+
+// const url = process.env.NEST_API
+
+// export const AXIOS_INSTANCE = Axios.create({
+//   baseURL: url,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// })
+
+// export const customInstance = <T>(
+//   config: AxiosRequestConfig,
+//   options?: AxiosRequestConfig,
+// ): Promise<T> => {
+//   return AXIOS_INSTANCE({
+//     ...config,
+//     ...options,
+//   }).then(({ data }) => data)
+// }
+
+// export type ErrorType<Error> = AxiosError<Error>
+
+// export type BodyType<Data> = Data
+
 import Axios, { AxiosError, AxiosRequestConfig } from 'axios'
 
-const url = process.env.NEST_API
-export const AXIOS_INSTANCE = Axios.create({
-  baseURL: url?.toString(),
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
+export const AXIOS_INSTANCE = Axios.create({ baseURL: 'https://sore-gray-pelican-wear.cyclic.app' })
 
 export const customInstance = <T>(
   config: AxiosRequestConfig,
   options?: AxiosRequestConfig,
 ): Promise<T> => {
-  return AXIOS_INSTANCE({
+  const source = Axios.CancelToken.source()
+  const promise = AXIOS_INSTANCE({
     ...config,
     ...options,
+    cancelToken: source.token,
   }).then(({ data }) => data)
+
+  // @ts-ignore
+  promise.cancel = () => {
+    source.cancel('Query was cancelled')
+  }
+
+  return promise
 }
 
 export type ErrorType<Error> = AxiosError<Error>
 
-export type BodyType<Data> = Data
+export type BodyType<BodyData> = BodyData
+
+// Or, in case you want to wrap the body type (optional)
+// (if the custom instance is processing data before sending it, like changing the case for example)
+//  export type BodyType<BodyData> = CamelCase<BodyData>;
