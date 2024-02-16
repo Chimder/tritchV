6436aef7caf226687733b51dd087d74f6db296b3
Chimder <127688661+Chimder@users.noreply.github.com>
2024-02-16 16:14:58 +0200

    del cookie

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -3,26 +3,35 @@ import {
   authControllerSingIn,
   authControllerSingOut,
 } from '@/shared/api/generated'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 
 import { Button } from '@/components/ui/button'
 
+function deleteTokenCookie() {
+  document.cookie =
+    'access-token-tritch=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=None; Secure;'
+}
 const Auth = () => {
+  const queryClient = useQueryClient()
   const { mutate } = useMutation({
     mutationKey: ['in'],
     mutationFn: ({ email, password }: { email: string; password: string }) =>
       authControllerSingIn({ email: email, password: password }),
   })
-  // const { data } = useQuery({
-  //   queryKey: ['login'],
-  //   queryFn: () => authControllerGetSessionInfo(),
-  //   retry: 0,
-  //   staleTime: 30 * 60 * 1000,
-  // })
+  const { data } = useQuery({
+    queryKey: ['login'],
+    queryFn: () => authControllerGetSessionInfo(),
+    retry: 0,
+    staleTime: 1 * 60 * 1000,
+  })
   const { mutate: out } = useMutation({
     mutationKey: ['ss'],
     mutationFn: () => authControllerSingOut(),
+    onSuccess() {
+      queryClient.removeQueries({ queryKey: ['in'], exact: true })
+      deleteTokenCookie()
+    },
   })
   return (
     <>
@@ -35,7 +44,7 @@ const Auth = () => {
       <Button className="mt-60" onClick={() => out()}>
         out
       </Button>
-      <div className="mt-65 text-white"> lol</div>
+      <div className="mt-65 text-white">{data?.email} lol</div>
     </>
   )
 }
