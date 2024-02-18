7af307466ce432e2fa375bd8f108786953886446
Chimder <127688661+Chimder@users.noreply.github.com>
2024-02-18 22:17:36 +0200

    sess

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
-
 import Providers from './providers.tsx'
 import Routes from './routes/routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Providers>
-      <Routes />
-    </Providers>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Providers>
+    <Routes />
+  </Providers>,
+  // </React.StrictMode>,
 )
