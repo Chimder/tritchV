13173e05dea05e90b576d7e1b8925339e7d5ba21
Chimder <dimas.kirilyuk@gmail.com>
2024-09-18 17:17:57 +0300

    rsbuild

---
+++ rsbuild.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+
+const { publicVars } = loadEnv({ prefixes: ['VITE_'] })
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    template: './index.html',
+  },
+  source: {
+    define: publicVars,
+    entry: {
+      index: './src/app/main.tsx',
+    },
+  },
+})
